74aeb450a39e2be0139246ab7dc8f824cd4a6347
DanielBradleyBennett <danielbradleybennett@gmail.com>
2020-07-26 17:47:36 -0500

    message works and can clear message

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,44 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes'
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-// export class HeroesComponent implements OnInit {
-//   // hero: Hero = {
-//   //   id: 1,
-//   //   name: 'Windstorm'
-//   // };
-//   heroes = HEROES;
-//   selectedHero: Hero;
-
-//   constructor() { }
-
-//   ngOnInit(): {
-//   }
-//   onSelect(hero: Hero): void {
-//     this.selectedHero = hero;
-
-//   }
-
-// }
 
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+
 
   }
   getHeroes(): void {
