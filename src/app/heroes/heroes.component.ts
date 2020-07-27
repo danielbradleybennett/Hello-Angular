e51817afd6e930f091db8e94af792985acaff0df
DanielBradleyBennett <danielbradleybennett@gmail.com>
2020-07-26 19:19:36 -0500

    routing works

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,23 +8,15 @@ import { MessageService } from '../message.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-
 export class HeroesComponent implements OnInit {
-
-  heroes: Hero[];
-  selectedHero: Hero;
+  heroes: Hero[]
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
 
-
-  }
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
